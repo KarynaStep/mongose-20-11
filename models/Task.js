4c0a93523fdf0708c24d174c98900f29ccbae4a9
KarynaStep <stepanova.carina98@gmail.com>
2023-11-22 18:57:28 +0200

    Initial commit

--- models/Task.js
+++ models/Task.js
@@ -1,46 +1,44 @@
-
-const {isAfter} = require('date-fns')
+const { isAfter } = require("date-fns");
 const mongoose = require("mongoose");
-const { contentSchema, emailSchema } = require('../utils/validationSchema');
-
+const { contentSchema, emailSchema } = require("../utils/validationSchema");
 const { Schema } = mongoose;
 
-
-
-
-const taskSchema = new Schema({
-  content: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (value) => contentSchema.isValid(value),
-      message: (props) => `${props.value} is bad content!`,
-    },
-  },
-  isDone: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now },
-  deadLine: {
-    type: Date,
-    validate: { validator: (value) => isAfter(value, Date.now()) },
-  },
-  owner: {
-    name: { type: String, required: true },
-    email: {
+const taskSchema = new Schema(
+  {
+    content: {
       type: String,
       required: true,
-      validate: { validator: (value) => emailSchema.isValid(value) },
-    },
-    reiting: {
-      type: Number,
       validate: {
-        validator: (value) => {
-          value > 0 && value <= 10;
-        },
+        validator: (value) => contentSchema.isValid(value),
+        message: (props) => `${props.value} is bad content!`,
       },
     },
+    isDone: { type: Boolean, default: false },
+    deadLine: {
+      type: Date,
+      validate: { validator: (value) => isAfter(value, Date.now()) },
+    },
+    owner: {
+      name: { type: String, required: true },
+      email: {
+        type: String,
+        required: true,
+        validate: { validator: (value) => emailSchema.isValid(value) },
+      },
+      raiting: {
+        type: Number,
+        min: 1,
+        max: 5,
+        default: 1,
+      },
+    },
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
   },
-});
-
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
 const Task = mongoose.model("Task", taskSchema);
 
 module.exports = Task;
